030f3f2f327988873ccf1e705e6d284712a37e05
Birol <b.efe@compuccino.com>
2018-08-31 10:40:53 +0200

    Sauberes Basic Standard Startprojekt (wiederverwendbar) mit Bootstrap

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
-  meineTsVariable = 'Inhalter meiner TS-Variablen';
+  //Variablen
 }
