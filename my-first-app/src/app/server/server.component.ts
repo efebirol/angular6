631367db6d6da08c3f2ffe7ac7d7d2c6ff45af95
Birol <b.efe@compuccino.com>
2018-09-17 15:14:32 +0200

    Adding a new component

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//Konfiguriere der Komponente
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+//die eigentliche Klasse
+export class ServerComponent{
+
+}
