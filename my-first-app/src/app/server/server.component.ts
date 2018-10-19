a95c1394074d296518d96264173edec6c5609594
Birol <b.efe@compuccino.com>
2018-10-19 14:08:18 +0200

    Inline Template Code - Template Code im Controller (.ts) definieren

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from "@angular/core";
 //Konfiguriere der Komponente
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    // templateUrl: './server.component.html'
+    template: `
+    <app-server></app-server>
+    <app-server></app-server>
+    `
 })
 
 //die eigentliche Klasse
